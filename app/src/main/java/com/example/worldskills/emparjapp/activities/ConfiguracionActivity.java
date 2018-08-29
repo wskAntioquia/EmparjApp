81f9cbb4a54d06c9f94d90c59bb80150c7eb5fae
sydevelopers <sydevelopers2018@gmail.com>
2018-08-29 15:58:03 -0500

    first

--- app/src/main/java/com/example/worldskills/emparjapp/activities/ConfiguracionActivity.java
+++ app/src/main/java/com/example/worldskills/emparjapp/activities/ConfiguracionActivity.java
@@ -1,15 +1,59 @@
 package com.example.worldskills.emparjapp.activities;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 import com.example.worldskills.emparjapp.R;
 
 public class ConfiguracionActivity extends AppCompatActivity {
 
+    private Button btn_jugar;
+    private TextInputEditText tiempo;
+    private SharedPreferences preferences;
+    private RadioButton radioConTiempo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracion);
+        preferences=getSharedPreferences("tiempoJuego",MODE_PRIVATE);
+        recuperarConfiguracion();
+        btn_jugar=findViewById(R.id.btn_jugar_configuracion);
+        tiempo=findViewById(R.id.txt_tiempo);
+        radioConTiempo =findViewById(R.id.radio_con_tiempo);
+
+
+
+        btn_jugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //validar campo
+                if (radioConTiempo.isChecked()){
+                    guardarConfiguracion();
+                    Intent intent=new Intent(getApplicationContext(),JuegoConfiguracionActivity.class);
+                    intent.putExtra("tiempo",tiempo.getText().toString());
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    private void recuperarConfiguracion() {
+        String time =preferences.getString("tiempo",null);
+        if (time!=null){
+            tiempo.setText(time);
+        }
+    }
+
+    private void guardarConfiguracion() {
+        SharedPreferences.Editor editor=preferences.edit();
+        editor.putString("tiempo",tiempo.getText().toString());
+        editor.apply();
     }
 }
--- app/src/main/java/com/example/worldskills/emparjapp/data/Datos.java
+++ app/src/main/java/com/example/worldskills/emparjapp/data/Datos.java
@@ -1,8 +1,11 @@
 package com.example.worldskills.emparjapp.data;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.worldskills.emparjapp.models.Puntaje;
 
 public class Datos extends SQLiteOpenHelper {
     SQLiteDatabase bd;
@@ -12,12 +15,23 @@ public class Datos extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String SQL_QUERY="CREATE TABLE ";
+        db.execSQL(Constantes.SQL_QUERY);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + Constantes.TBL_PUNTAJES);
+        onCreate(db);
+    }
 
+    public boolean guardarDatosJuego(Puntaje puntaje){
+        bd=getWritableDatabase();
+        ContentValues values=new ContentValues();
+        values.put(Constantes.CAMPO_NOMBRE,puntaje.getNombre());
+        values.put(Constantes.CAMPO_PUNTOS,puntaje.getNombre());
+        values.put(Constantes.CAMPO_NIVEL,puntaje.getNombre());
+        values.put(Constantes.CAMPO_TIEMPO,puntaje.getNombre());
+        return bd.insert(Constantes.TBL_PUNTAJES,null,values)>0;
     }
 }
