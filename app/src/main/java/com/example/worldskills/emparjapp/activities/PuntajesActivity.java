109b3d679d59276b8890c6aa80ec976e8bda946b
sydevelopers <sydevelopers2018@gmail.com>
2018-08-29 17:57:17 -0500

    first

--- app/src/main/java/com/example/worldskills/emparjapp/activities/PuntajesActivity.java
+++ app/src/main/java/com/example/worldskills/emparjapp/activities/PuntajesActivity.java
@@ -1,15 +1,55 @@
 package com.example.worldskills.emparjapp.activities;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 import com.example.worldskills.emparjapp.R;
+import com.example.worldskills.emparjapp.data.Constantes;
+import com.example.worldskills.emparjapp.data.Datos;
+
+import java.util.ArrayList;
 
 public class PuntajesActivity extends AppCompatActivity {
+    private ListView facil,medio,dificil;
+    private Datos datos =new Datos(this);
+    private Cursor cursor;
+    private ListView[] arrayLists={facil,medio,dificil};
+    private int camposUI[]={R.id.contentNombre,R.id.contentPuntos};
+    private String camposBD[]={Constantes.CAMPO_NOMBRE,Constantes.CAMPO_PUNTOS};
+    private SimpleCursorAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puntajes);
+
+        facil=findViewById(R.id.listaFacil);
+        medio=findViewById(R.id.listaMedio);
+        dificil=findViewById(R.id.listaDificil);
+        cargarListas(arrayLists);
     }
+
+    private void cargarListas(ListView[] arrayLists) {
+       if (arrayLists.equals("facil")){
+           cursor=datos.listarJuegoFacil();
+           adapter=new SimpleCursorAdapter(this,R.layout.content_item_list,cursor,camposBD,camposUI);
+           facil.setAdapter(adapter);
+       }else if (arrayLists.equals("medio")){
+           cursor=datos.listarJuegoMedio();
+           adapter=new SimpleCursorAdapter(this,R.layout.content_item_list,cursor,camposBD,camposUI);
+           medio.setAdapter(adapter);
+       }else if (arrayLists.equals("dificil")){
+           cursor=datos.listarJuegoDificil();
+           adapter=new SimpleCursorAdapter(this,R.layout.content_item_list,cursor,camposBD,camposUI);
+           dificil.setAdapter(adapter);
+       }
+
+
+    }
+
+
 }
--- app/src/main/java/com/example/worldskills/emparjapp/data/Datos.java
+++ app/src/main/java/com/example/worldskills/emparjapp/data/Datos.java
@@ -2,13 +2,15 @@ package com.example.worldskills.emparjapp.data;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.example.worldskills.emparjapp.models.Puntaje;
 
 public class Datos extends SQLiteOpenHelper {
-    SQLiteDatabase bd;
+    private SQLiteDatabase bd;
+    private Cursor cursor;
     public Datos(Context context) {
         super(context, "emparej", null, 1);
     }
@@ -29,9 +31,46 @@ public class Datos extends SQLiteOpenHelper {
         bd=getWritableDatabase();
         ContentValues values=new ContentValues();
         values.put(Constantes.CAMPO_NOMBRE,puntaje.getNombre());
-        values.put(Constantes.CAMPO_PUNTOS,puntaje.getNombre());
-        values.put(Constantes.CAMPO_NIVEL,puntaje.getNombre());
-        values.put(Constantes.CAMPO_TIEMPO,puntaje.getNombre());
+        values.put(Constantes.CAMPO_PUNTOS,puntaje.getPuntos());
+        values.put(Constantes.CAMPO_NIVEL,puntaje.getNivel());
+        values.put(Constantes.CAMPO_TIEMPO,puntaje.getTiempo());
         return bd.insert(Constantes.TBL_PUNTAJES,null,values)>0;
+    }
+
+    public Cursor listarJuegoFacil(){
+        bd=getReadableDatabase();
+        cursor=bd.rawQuery("SELECT * FROM " + Constantes.TBL_PUNTAJES + " WHERE " +
+                                Constantes.CAMPO_NIVEL + " = "+ "'facil'" + " ORDER BY "+
+                                Constantes.CAMPO_PUNTOS + " DESC LIMIT 0,4",null);
+        if (cursor.moveToFirst()){
+            return cursor;
+        }else {
+            return null;
+        }
+
+    }
+
+    public Cursor listarJuegoMedio(){
+        bd=getReadableDatabase();
+        cursor=bd.rawQuery("SELECT * FROM " + Constantes.TBL_PUNTAJES + " WHERE " +
+                Constantes.CAMPO_NIVEL + " = "+ "'medio'" + " ORDER BY "+
+                Constantes.CAMPO_PUNTOS + " DESC LIMIT 0,4",null);
+        if (cursor.moveToFirst()){
+            return cursor;
+        }else {
+            return null;
+        }
+    }
+
+    public Cursor listarJuegoDificil(){
+        bd=getReadableDatabase();
+        cursor=bd.rawQuery("SELECT * FROM " + Constantes.TBL_PUNTAJES + " WHERE " +
+                Constantes.CAMPO_NIVEL + " = "+ "'dificil'" + " ORDER BY "+
+                Constantes.CAMPO_PUNTOS + " DESC LIMIT 0,4",null);
+        if (cursor.moveToFirst()){
+            return cursor;
+        }else {
+            return null;
+        }
     }
 }
